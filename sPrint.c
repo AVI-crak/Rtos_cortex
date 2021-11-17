f80db85087bcf6037b80d94aac86be695d31bf5e
AVI-crak <35156298+AVI-crak@users.noreply.github.com>
2021-11-18 01:45:27 +0600

    Add files via upload
    Ускорение операций деления

--- sPrint.c
+++ sPrint.c
@@ -1,9 +1,9 @@
 /// sPrint.c
-/// –ø–µ—á–∞—Ç—å –±–µ–∑ —É–∫–∞–∑–∞–Ω–∏—è —Ç–∏–ø–∞ –ø–∞—Ä–∞–º–µ—Ç—Ä–∞, –≤ —Ä–∞–∑–Ω–æ–±–æ–π
-/// –±–µ–∑ –∑–∞–≤–∏—Å–∏–º–æ—Å—Ç–µ–π –æ—Ç –≤–Ω–µ—à–Ω–∏—Ö –±–∏–±–ª–∏–æ—Ç–µ–∫
-/// printo("—Ç–µ–∫—Å—Ç", double / float / uint(8-32-64)_t / int(8-32-64)_t )
-/// —Ä–∞–∑–º–µ—Ä - 1907 –±–∞–π—Ç –ø—Ä–∏ –∞–≥—Ä–µ—Å—Å–∏–≤–Ω–æ–π –æ–ø—Ç–∏–º–∏–∑–∞—Ü–∏–∏
-/// –Ω–∞ –≤–∫—É—Å –∏ —Ü–≤–µ—Ç... –¥–æ–±–∞–≤–∏—Ç—å —Å–æ–±—Å—Ç–≤–µ–Ω–Ω—É—é —Ñ—É–Ω–∫—Ü–∏—é –ø–µ—á–∞—Ç–∏ –≤
+/// ÔÂ˜‡Ú¸ ·ÂÁ ÛÍ‡Á‡ÌËˇ ÚËÔ‡ Ô‡‡ÏÂÚ‡, ‚ ‡ÁÌÓ·ÓÈ
+/// ·ÂÁ Á‡‚ËÒËÏÓÒÚÂÈ ÓÚ ‚ÌÂ¯ÌËı ·Ë·ÎËÓÚÂÍ
+/// printo("ÚÂÍÒÚ", double / float / uint(8-32-64)_t / int(8-32-64)_t )
+/// ‡ÁÏÂ - 1907 ·‡ÈÚ ÔË ‡„ÂÒÒË‚ÌÓÈ ÓÔÚËÏËÁ‡ˆËË
+/// Ì‡ ‚ÍÛÒ Ë ˆ‚ÂÚ... ‰Ó·‡‚ËÚ¸ ÒÓ·ÒÚ‚ÂÌÌÛ˛ ÙÛÌÍˆË˛ ÔÂ˜‡ÚË ‚
 /// soft_print()
 
 #include "sPrint.h"
@@ -35,98 +35,127 @@ union double_raw
     double      d_raw;
 };
 
+union divrev
+{
+    uint64_t w64;
+    uint32_t w32[2];
+};
+
 const int16_t data_otner[129] = {
--308,-304,-299,-294,-289,-284,-280,-275,-270,-265,-260,-255,-251,-246,-241,-236,-231,-227,-222,-217,-212,
--207,-202,-198,-193,-188,-183,-178,-174,-169,-164,-159,-154,-150,-145,-140,-135,-130,-125,-121,-116,-111,
--106,-101,-97,-92,-87,-82,-77,-72,-68,-63,-58,-53,-48,-44,-39,-34,-29,-24,-19,-15,-10,-5,0,5,9,14,19,24,
-29,34,38,43,48,53,58,62,67,72,77,82,86,91,96,101,106,111,115,120,125,130,135,139,144,149,154,159,164,168,
-173,178,183,188,192,197,202,207,212,217,221,226,231,236,241,245,250,255,260,265,270,274,279,284,289,294,
-298,303,308};
+-308,-304,-299,-294,-289,-284,-280,-275,-270,-265,-260,-256,-251,-246,-241,-236,-231,-227,-222,-217,-212,
+-207,-203,-198,-193,-188,-183,-178,-174,-169,-164,-159,-154,-150,-145,-140,-135,-130,-125,-121,-116,-111,
+-106,-101,-97,-92,-87,-82,-77,-72,-68,-63,-58,-53,-48,-44,-39,-34,-29,-24,-19,-15,-10,
+-5,0,5,9,14,19,24,29,33,38,43,48,53,58,62,67,72,77,82,86,91,
+96,101,106,111,115,120,125,130,135,139,144,149,154,159,164,168,173,178,183,188,192,
+197,202,207,212,217,221,226,231,236,241,245,250,255,260,265,269,274,279,284,289,294,
+298,303,308,};
 
 const uint32_t data_of10raw[129] = {
 222507385,1458224403,955661945,626302612,410453680,268994923,1762885132,1155324400,757153399,496208051,
-325194908,213119735,1396701497,915342293,599878725,393136521,257645950,1688508503,1106580932,725208879,
-475272891,311474842,204128152,1337774260,876723739,574569669,376549978,246775794,1617269844,1059893965,
+325194908,2131197353,1396701497,915342293,599878725,393136521,257645950,1688508503,1106580932,725208879,
+475272891,311474842,2041281525,1337774260,876723739,574569669,376549978,246775794,1617269844,1059893965,
 694612109,455220991,298333629,1955159272,1281333180,839734513,550328410,360663227,236364252,1549036765,
 1015176734,665306225,436015087,285746847,1872670541,1227273366,804305873,527109897,345446742,226391976,
 1483682460,972346137,637236764,417619485,273691106,1793662034,1175494350,770371977,504870979,330872245,
 216840434,1421085471,931322574,610351562,400000000,262144000,1717986918,1125899906,737869762,483570327,
-316912650,207691874,1361129467,892029807,584600654,383123885,251084069,1645504557,1078397866,706738825,
+316912650,2076918743,1361129467,892029807,584600654,383123885,251084069,1645504557,1078397866,706738825,
 463168356,303542014,1989292945,1303703024,854394814,559936185,366959778,240490760,1576080247,1032899951,
 676921312,443627151,290735489,1905364105,1248699420,818347651,536312317,351477640,230344386,1509584969,
 989321605,648361807,424910394,278469275,1824976247,1196016433,783821329,513685146,336648697,220626090,
 1445895146,947581843,621007236,406983302,266720577,1747979975,1145556156,750751682,492012622,322445392,
-211317812,1384892415,907603093,594806763,389812560,255467559,1674232198,1097224813,719077253};
+2113178124,1384892415,907603093,594806763,389812560,255467559,1674232198,1097224813,719077253,};
+
+
 
 const char txt_NaN[] = "NaN";
-char* floating_char(uint32_t massa, uint32_t of10raw, int16_t feeze, int16_t order10, char* txt);
+char* floating_char(uint32_t massa, uint32_t of10raw, int32_t feeze, int32_t order10, char* txt);
 
-char* nex_char(uint32_t value, char* text)  /// 68  –±–∞–π—Ç
-{
-    int32_t tmp;
-    volatile char* tex; tex = text;
-    *(tex--) = 0;
-    do
-    {
-        tmp = value & 0x0F;
-        if (tmp > 9 ) *(tex--) = tmp + 55;
-            else *(tex--) = tmp + '0';
-        value >>= 4;
-    }while (value);
-    *(tex--) = 'x';
-    *tex = '0';
-    return tex;
-};
 
-char * i32_char(int32_t value, char* text)  /// 108  –±–∞–π—Ç
+__attribute__ ((optimize("-Os"))) char* hex_char(uint32_t value, char* tail_txt)  /// 60  ·‡ÈÚ
 {
-    if (value < 0)
-    {
-        value = 0 - value;
-        text = u32_char(value, text);
-        *(--text) = '-';
-    }else text = u32_char(value, text);
-    return text;
-};
-
-char* u32_char (uint32_t value, char* text)  /// 52  –±–∞–π—Ç
-{
-    *text = 0;
+    int32_t tmp, jump; jump = 0;
+    *(tail_txt--) = 0;
     do{
-        *(--text) = (value % 10) + '0';
-        value /= 10;
-    }while (value != 0);
-    return text;
+        tmp = value & 0x0F;
+        if (tmp > 9 ) *(tail_txt--) = tmp + 55;
+            else *(tail_txt--) = tmp + '0';
+        value >>= 4; jump++;
+    }while ((value )||(jump & 1));
+    *(volatile char*)tail_txt-- = 'x';
+    *(volatile char*)tail_txt = '0';
+    return tail_txt;
 };
 
-char* i64_char(int64_t value, char* text)
+__attribute__ ((optimize("-Os"))) char * i32_char(int32_t value, char* tail_txt)  /// 32  ·‡ÈÚ
 {
-    if (value < 0)
-    {
+    if (value < 0){
         value = 0 - value;
-        text = u64_char(value, text);
-        *(--text) = '-';
-    }else text = u64_char(value, text);
-    return text;
+        tail_txt = u32_char(value, tail_txt);
+        *(--tail_txt) = '-';
+    }else tail_txt = u32_char(value, tail_txt);
+    return tail_txt;
 };
 
-char* u64_char(uint64_t value, char* text) /// 108  –±–∞–π—Ç
+__attribute__ ((optimize("-Os"))) char* u32_char (uint32_t value, char* tail_txt) /// 40 ·‡ÈÚ
 {
-    if ((value >> 32) == 0)
-    {
-        text = u32_char(value, text);
-    }else
-    {
-        *text = 0;
-        do{
-            *(--text) = (value % 10) + '0';
-            value /= 10;
-        }while (value != 0);
-    };
-    return text;
+    *tail_txt = 0;
+    uint32_t res, tmp;
+    do{
+        tmp = ((uint64_t) value * 3435973837UL >> 32);
+        res = value + '0';
+        value = tmp >> 3;
+        res -= value * 10;
+        *(--tail_txt) = res;
+    }while (value);
+    return tail_txt;
 };
 
-void compress_char(char* tex_in, char* tex_out) /// 60  –±–∞–π—Ç
+__attribute__ ((optimize("-Os"))) char* i64_char(int64_t value, char* tail_txt)
+{
+    if (value < 0){
+        value = 0 - value;
+        tail_txt = u64_char(value, tail_txt);
+        *(--tail_txt) = '-';
+    }else tail_txt = u64_char(value, tail_txt);
+    return tail_txt;
+};
+
+__attribute__ ((optimize("-Os")))char* u64_char (uint64_t value, char* tail_txt) /// 112 ·‡ÈÚ
+{
+    uint32_t res, t10, mag;
+    union divrev rev;
+    union divrev rew;
+    union divrev reg;
+    rev.w64 = value;
+    *tail_txt = 0;
+    mag = 3435973837UL; t10 = 10;
+    do{
+        rew.w32[1] = ((uint64_t)rev.w32[1] * mag >> 35);
+        rew.w32[0] = 0;
+        reg.w64 = (uint64_t)rev.w64 - rew.w64 * t10;
+        res = ((uint64_t)reg.w64 >> 8);
+        res = ((uint64_t)res * mag >> 35);
+        rew.w64 += (uint64_t)res << 8;
+        reg.w64 = (uint64_t)rev.w64 - rew.w64 * t10;
+        res = ((uint64_t)reg.w32[0] * mag >> 35);
+        rew.w64 += (uint64_t)res;
+        reg.w32[0] += '0';
+        res = (reg.w32[0] - res * t10);
+        *(--tail_txt) = res;
+        rev.w64 = (uint64_t)rew.w64;
+    }while (rev.w32[1]);
+    if (rev.w32[0] == 0) return tail_txt;
+    do{
+        rev.w32[1] = ((uint64_t)rev.w32[0] * mag >> 32);
+        res = rev.w32[0] + '0';
+        rev.w32[0] = rev.w32[1] >> 3;
+        res -=  rev.w32[0] * t10;
+        *(--tail_txt) = res;
+    }while (rev.w32[0]);
+    return tail_txt;
+};
+
+__attribute__ ((optimize("-Os"))) void compress_char(char* tex_in, char* tex_out) /// 60  ·‡ÈÚ
 {
     int32_t ovr;
     ovr =  (tex_out - tex_in) - OUT_TXT_SIZE_FLOATING;
@@ -148,7 +177,7 @@ void compress_char(char* tex_in, char* tex_out) /// 60  –±–∞–π—Ç
     };
 };
 
-char* float_char(float value, char* text) /// 200 –±–∞–π—Ç
+__attribute__ ((optimize("-Os"))) char* float_char(float value, char* text) /// 200 ·‡ÈÚ
 {
     union float_raw    ftemp;
     ftemp.f_raw = value;
@@ -188,7 +217,7 @@ char* float_char(float value, char* text) /// 200 –±–∞–π—Ç
         while (ftemp.sign == 0)
         {
             of10raw >>= 1; ftemp.u_raw <<= 1;
-            if (of10raw < 200000000 )
+            if (of10raw < 214748364)
                 {
                     order10 -= 1;
                     of10raw *=10;
@@ -205,14 +234,14 @@ char* float_char(float value, char* text) /// 200 –±–∞–π—Ç
     return text;
 };
 
-char* double_char(double value, char* text)    /// 252 –±–∞–π—Ç
+__attribute__ ((optimize("-Os"))) char* double_char(double value, char* text)    /// 252 ·‡ÈÚ
 {
     union double_raw    dtemp;
     dtemp.d_raw = value;
     int_fast8_t sign;
-    uint16_t order;
+    uint32_t order;
     uint32_t massa, of10raw;
-    int16_t feeze, order10, cis;
+    int32_t feeze, order10, cis;
     sign = dtemp.sign; order = dtemp.order;
 
     feeze = 0;
@@ -244,7 +273,7 @@ char* double_char(double value, char* text)    /// 252 –±–∞–π—Ç
         while (dtemp.sign == 0)
         {
             of10raw >>= 1; dtemp.u64_raw <<= 1;
-            if (of10raw < 200000000 )
+            if (of10raw < 214748364)
                 {
                     order10 -= 1;
                     of10raw *=10;
@@ -262,45 +291,33 @@ char* double_char(double value, char* text)    /// 252 –±–∞–π—Ç
     return text;
 };
 
-char* floating_char(uint32_t massa, uint32_t of10raw, int16_t feeze, int16_t order10, char* txt) /// 304 –±–∞–π—Ç
+__attribute__ ((optimize("-Os"))) char* floating_char(uint32_t massa, uint32_t of10raw, int32_t feeze, int32_t order10, char* txt) /// 304 ·‡ÈÚ
 {
     if (feeze > 7)
     {
         while ( (feeze++) != 16)
         {
             of10raw >>= 1;
-            if (of10raw < 200000000 )
-            {
-                 order10 -= 1; of10raw *=10;
-            };
+            if (of10raw < 214748364)
+                {order10 -= 1; of10raw *=10;};
         };
     }else
     {
         while ( (feeze--) != 0)
         {
             of10raw <<= 1;
-            if (of10raw > 2000000000 )
-                {
-                    order10 += 1; of10raw /=10;
-                };
+            if ((of10raw >> 31) == 1)
+                {order10 += 1;
+                 of10raw = ((uint64_t) of10raw * 3435973837UL >> 35);};
         };
     };
-    union double_raw of10;
-    of10.u64_raw = (uint64_t) massa * of10raw;
+    massa = ((uint64_t) massa * of10raw >> 32);
     char *mas_s, *mas_f, *ord_s, *ord_f; int32_t tmp, tff;
     mas_f = txt + 12;
-    mas_s = u32_char (of10.u_raw[1], mas_f);
-    of10.u_raw[1] = of10.u_raw[0] / 429496729;
-    *(mas_f++) = of10.u_raw[1] + '0';
-    of10.u_raw[0] = of10.u_raw[0] - (of10.u_raw[1] * 429496729);
-    of10.u_raw[1] = of10.u_raw[0] / 42949672;
-    *(mas_f++) = of10.u_raw[1] + '0';
-    of10.u_raw[0] = of10.u_raw[0] - (of10.u_raw[1] * 42949672);
-    of10.u_raw[1] = of10.u_raw[0] / 4294967;
-    *(mas_f++) = of10.u_raw[1] + '0';
-    *mas_f = 0;
+    mas_s = u32_char (massa, mas_f);
 
-    tmp = mas_f - mas_s; tmp -= 12;
+
+    tmp = mas_f - mas_s; tmp -= 9;
     order10 += tmp;
     if ((-12 <= order10)&&(order10 < 15))
     {
@@ -309,7 +326,7 @@ char* floating_char(uint32_t massa, uint32_t of10raw, int16_t feeze, int16_t ord
     } else tff = 0;
     if (order10 != 0)
     {
-        ord_f = txt + 23;
+        ord_f = txt + 19;
         ord_s = i32_char(order10, ord_f);
         *(--ord_s) = 'e';
         tmp = ord_f - ord_s;
@@ -328,12 +345,12 @@ char* floating_char(uint32_t massa, uint32_t of10raw, int16_t feeze, int16_t ord
     return txt;
 };
 
+///-----------------------------------------------------------------------
 
 
-/*
-///  858993459200000000
-/// 8589934592000000000
- void tabl_grabl(void)
+
+/**
+void tabl_grabl(void)
 {
     volatile uint32_t cis;
     volatile uint64_t of10raw;
@@ -341,48 +358,42 @@ char* floating_char(uint32_t massa, uint32_t of10raw, int16_t feeze, int16_t ord
     volatile int32_t cis2;
     volatile uint32_t cis3;
     volatile int32_t cis4;
-    cis4 = -1;
+    volatile int32_t cis5;
+    printo("\n\n");
+
+    cis5 = 2;
+    while (cis5--){
+    if (cis5 == 1)printo("const int16_t data_otner[129] = { \n");
+    if (cis5 == 0)printo("const uint32_t data_of10raw[129] = { \n");
+    cis4 = 0;
     for(cis2 = 0; cis2 !=129; cis2++)
     {
         cis = cis2 <<4;
         of10raw = 858993459200000000;
         ofreze = 0;
-    if (cis > 1023)
-    {
-        while ( (cis--) != 1023){
-            of10raw <<= 1;
-            if (of10raw > 8589934592000000000 )
-                {
-                    ofreze += 1;
-                    of10raw /=10;
-                };
-        };
-    } else if (cis < 1023)
-    {
-        while ( (cis++) != 1023){
-            of10raw >>= 1;
-            if (of10raw < 858993459200000000 )
-                {     //
-                    ofreze -= 1;
-                    of10raw *=10;
-                };
-        };
+        if (cis > 1023){
+            while ( (cis--) != 1023){
+                of10raw <<= 1;
+                if ((of10raw >> 63 ) !=0){
+                    ofreze += 1; of10raw /=10;};};}
+        else if (cis < 1023){
+            while ( (cis++) != 1023){
+                of10raw >>= 1;
+                if (of10raw < 922337200250290176){
+                    ofreze -= 1; of10raw *=10;};};};
+        cis3 = (uint32_t) (of10raw >> 32);
+        if (cis5 == 1){cis4++;if (cis4 > 19) {cis4 = 0; printo("\n");} else; printo(ofreze,",");  };
+        if (cis5 == 0){cis4++;if (cis4 > 9){cis4 = 0;printo("\n");}else printo( cis3,",");};
+    }; printo( "};\n\n");
     };
-    cis3 = (uint32_t) (of10raw >> 32);
-    cis4++;
-    if (cis4 > 9) {cis4 = 0; printo("\n");}; printo( cis3,",");
-   // if (cis4 > 20) {cis4 = 0; printo("\n");}; printo(ofreze,",");
-  //
-    };
-
-os_Delay_ms(20000);
-
 };
+**/
 
-*/
 ///-------------------------
-/*
+/**
 printo ("\f");
+printo("\n 16777216.0f         = ",16777216.0f);
+printo("\n -16777216.0f        = ",-16777216.0f);
 printo("\n 5.64231125e+5f = ", 5.64231125e+5f);
 printo("\n -8.65399996416e+12f = ", -8.65399996416e+12f);
 printo("\n 9.99999949672e+20f  = ", 9.99999949672e+20f);
@@ -432,7 +443,7 @@ printo("\n -4 =                   ", -4L);
 
 
 
-printo("\n 4567891234567891234 =  ", 4567891234567891234ULL);
+printo("\n 18446744073709551614 = ", 18446744073709551614ULL);
 printo("\n 456789123456789123 =   ", 456789123456789123ULL);
 printo("\n 45678912345678912 =    ", 45678912345678912ULL);
 printo("\n 4567891234567891 =     ", 4567891234567891ULL);
@@ -442,14 +453,14 @@ printo("\n 4567891234567 =        ", 4567891234567ULL);
 printo("\n 456789123456 =         ", 456789123456ULL);
 printo("\n 45678912345 =          ", 45678912345ULL);
 printo("\n 4567891234 =           ", 4567891234ULL);
-printo("\n 456789123 =            ", 456789123UL);
-printo("\n 45678912 =             ", 45678912UL);
-printo("\n 4567891 =              ", 4567891UL);
-printo("\n 456789 =               ", 456789UL);
-printo("\n 45678 =                ", 4567UL);
-printo("\n 456 =                  ", 456UL);
-printo("\n 45 =                   ", 45UL);
-printo("\n 4 =                    ", 4UL);
+printo("\n 4294967295 =           ", 4294967295UL);
+printo("\n 429496729 =            ", 429496729UL);
+printo("\n 42949672 =             ", 42949672UL);
+printo("\n 4294967 =              ", 4294967UL);
+printo("\n 429496 =               ", 429496UL);
+printo("\n 42949 =                ", 42949UL);
+printo("\n 4294 =                 ", 4294UL);
+printo("\n 42 =                   ", 42UL,"\n");
 
 char ntst_texx[] = "helou words";
 printo(ntst_texx, "\n");
@@ -518,5 +529,9 @@ ui64temp4 = 18446744073709551614;
 printo("\n uint64_t 18446744073709551614  = ", ui64temp4);
 ui64temp4 = 1;
 printo("\n uint64_t 1                     = ", ui64temp4);
-*/
+
+
+**/
+
+
 
